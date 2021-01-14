6d984319117bb0bc473c00433e0663feeddb3240
Rocky <rockygitt@gmail.com>
2021-01-14 02:44:55 +0100

    AoC Problem 01

---
+++ day_01/main.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const getInput = async () => {
+  return new Promise(resolve => {
+    const lines = []
+
+    readline
+      .createInterface({ input: fs.createReadStream('input.txt') })
+      .on('line', line => lines.push(line))
+      .on('close', () => (lines.length === 1 ? resolve(lines[0]) : resolve(lines)))
+  })
+}
+
+const main = async () => {
+  const input = await getInput()
+  const parens = input.split('')
+
+  const result = parens.filter(p => p === '(').length - parens.filter(p => p === ')').length
+
+  let floor = 0
+  let i
+
+  for (i = 0; i < parens.length; i++) {
+    floor += parens[i] === '(' ? 1 : -1
+
+    if (floor === -1) break
+  }
+
+  console.log(`Santa's floor (1): ${result}`)
+  console.log(`First position where Santa enters the basement (2): ${i + 1}`)
+}
+
+main()
+
+module.exports = main

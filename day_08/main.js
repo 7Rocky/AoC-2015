8c258bb22b24d2671afb8f62661b2fc61c63373d
Rocky <rockygitt@gmail.com>
2021-01-21 02:31:32 +0100

    AoC Problem 08

---
+++ day_08/main.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const getInput = async () => {
+  return new Promise(resolve => {
+    const lines = []
+
+    readline
+      .createInterface({ input: fs.createReadStream('input.txt') })
+      .on('line', line => lines.push(line))
+      .on('close', () => (lines.length === 1 ? resolve(lines[0]) : resolve(lines)))
+  })
+}
+
+const sum = (t, n) => t + n
+
+const encodeCharacters = string => `"${string.replace(/\\/g, '\\\\').replace(/"/g, '\\"')}"`
+
+const main = async () => {
+  const input = await getInput()
+
+  const totalLengthCode = input.map(line => line.length).reduce(sum)
+  const totalLengthMemory = input.map(line => eval(line).length).reduce(sum)
+  const totalLengthEncoded = input.map(line => encodeCharacters(line).length).reduce(sum)
+
+  console.log(`Total code minus memory length (1): ${totalLengthCode - totalLengthMemory}`)
+  console.log(`Total encoded minus code length (2): ${totalLengthEncoded - totalLengthCode}`)
+}
+
+main()
